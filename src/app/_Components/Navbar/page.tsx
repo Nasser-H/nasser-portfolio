178d6db4ef0e53f7c5bedc9cb797ab2cd919d11e
Nasser-H <nasser.h0404@gmail.com>
2025-05-14 16:07:52 +0300

    Navbar has finshed

---
+++ src/app/_Components/Navbar/page.tsx
@@ -0,0 +1,19 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Navbar() {
+  return <>
+    <nav className='w-full px-4 sm:px-6 md:px-8 lg:py-6 lg:px-10 fixed z-50 sm:backdrop:blur'>
+        <div className='flex flex-col mt-4 lg:mt-0 items-center lg:flex-row justify-between w-full'>
+            <h1 className='uppercase text-main backdrop:blur text-2xl lg:text-xl'><Link href="/">Nasser Hussein</Link></h1>
+            <ul className='text-xl lg:text-lg text-white mt-5 lg:mt-0 flex gap-x-5'>
+                <li className='size-8 hover:border-main hover:border-2 hover:text-main duration-200 rounded-full flex justify-center items-center cursor-pointer'><a href="https://www.facebook.com/profile.php?id=100004291441295" target='_blank'><i className="fa-brands fa-facebook-f"></i></a></li>
+                <li className='size-8 hover:border-main hover:border-2 hover:text-main duration-200 rounded-full flex justify-center items-center cursor-pointer'><a href="http://wa.me/201061132684" target='_blank'><i className="fa-brands fa-whatsapp"></i></a></li>
+                <li className='size-8 hover:border-main hover:border-2 hover:text-main duration-200 rounded-full flex justify-center items-center cursor-pointer'><a href="mailto:nasser.h0404@gmail.com" target='_blank'><i className="fa-solid fa-at"></i></a></li>
+                <li className='size-8 hover:border-main hover:border-2 hover:text-main duration-200 rounded-full flex justify-center items-center cursor-pointer'><a href="https://github.com/Nasser-H" target='_blank'><i className="fa-brands fa-github"></i></a></li>
+                <li className='size-8 hover:border-main hover:border-2 hover:text-main duration-200 rounded-full flex justify-center items-center cursor-pointer'><a href="https://www.linkedin.com/in/nasser-hussein/" target='_blank'><i className="fa-brands fa-linkedin-in"></i></a></li>
+            </ul>
+        </div>
+    </nav>
+  </>
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors:{
+        main: '#707afe',
+        bgMain: '#1C1D2B'
+      }
+    },
+  },
+  plugins: [],
+}
+
