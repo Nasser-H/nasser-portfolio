df367d54f9f6e6f7b280cc966ed5ff074b2b59c5
Nasser-H <nasser.h0404@gmail.com>
2025-05-19 03:19:07 +0300

    solve broblem key

--- src/app/_components/navbar/page.tsx
+++ src/app/_components/navbar/page.tsx
@@ -17,7 +17,7 @@ export default function NavBar() {
   <nav className='fixed lg:right-[2%] bottom-0 w-full lg:w-auto lg:h-screen flex lg:flex-col justify-center z-40'>
     <ul className='text-white text-xl flex lg:block justify-center bg-bgSecond backdrop-blur-3xl w-full lg:py-8 px-8 lg:rounded-full rounded-t-xl lg:px-1 py-1 space-y-1'>
         {navs.map((nav : {text: string, link: string} ,index : number) =>
-              <Link href={nav.link}><li key={index} className={`size-14 ${pathName == nav.link ? 'bg-bgMain text-main -translate-y-1/2 lg:translate-y-0' : 'hover:bg-main'} rounded-full flex justify-center items-center cursor-pointer`}><i className={nav.text}></i></li></Link>
+              <Link  key={index} href={nav.link}><li className={`size-14 ${pathName == nav.link ? 'bg-bgMain text-main -translate-y-1/2 lg:translate-y-0' : 'hover:bg-main'} rounded-full flex justify-center items-center cursor-pointer`}><i className={nav.text}></i></li></Link>
         )}
         <Link href={''}><li className='size-14 hover:bg-main rounded-full flex justify-center items-center cursor-pointer'><span>En</span></li></Link>
     </ul>
