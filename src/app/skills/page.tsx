1029edee84b628b776771298dc61ca3d3028f26a
Nasser-H <nasser.h0404@gmail.com>
2025-06-03 20:33:16 +0300

    skills last update component

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -89,9 +89,9 @@ export default function Skills() {
   return <> 
     <div className='text-center mt-16 mb-10'>
         <h1 dir='rtl' className='mb-8 text-main text-3xl font-extralight'>المهارات</h1>
-        <div className='flex flex-wrap justify-start items-center w-[50%] mx-auto gap-y-10 mb-14'>
+        <div className='flex flex-wrap justify-start items-center w-[90%] sm:w-3/4 lg:w-1/2 mx-auto gap-y-10 mb-14'>
             {skills.map((skill, index)=>
-                <div key={index} className='text-white/80 flex justify-center items-center px-2 w-full sm:w-1/2 lg:w-1/3'>
+                <div key={index} className='text-white/80 flex justify-center items-center px-2 w-1/2 md:w-1/3'>
                     <div className='bg-bgSecond rounded-lg w-[146px] h-[150px] flex flex-col justify-center items-center space-y-4'>
                         <div className='size-[90px] rounded-full flex justify-center items-center' style={{ background:`conic-gradient(#ededed ${Math.round(360-skill.levelInPercentage*360/100)}deg, #707afe 0deg)` }}>
                             <div className='size-[75px] bg-bgMain text-white rounded-full flex justify-center items-center text-4xl'>
@@ -110,9 +110,9 @@ export default function Skills() {
             )}
         </div>
         <h1 dir='rtl' className='mb-8 text-main text-3xl font-extralight'>اللغات</h1>
-        <div className='flex flex-wrap justify-center items-center w-[50%] mx-auto gap-y-10'>
+        <div className='flex flex-wrap justify-center items-center w-[90%] sm:w-3/4 lg:w-1/2 mx-auto gap-y-10'>
             {languages.map((language, index)=>
-                <div key={index} className='text-white/80 flex justify-center items-center px-2 w-full sm:w-1/2 lg:w-1/3'>
+                <div key={index} className='text-white/80 flex justify-center items-center px-2 w-1/2 md:w-1/3'>
                     <div className='bg-bgSecond rounded-lg w-[146px] h-[150px] flex flex-col justify-center items-center space-y-4'>
                         <div className='size-[90px] rounded-full flex justify-center items-center' style={{ background:`conic-gradient(#ededed ${Math.round(360-language.levelInPercentage*360/100)}deg, #707afe 0deg)` }}>
                             <div className='size-[75px] bg-bgMain text-white rounded-full flex justify-center items-center text-4xl'>
